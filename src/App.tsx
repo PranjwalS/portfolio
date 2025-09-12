17cdc0bbfacf9c72c23ee5d3294df8292868392a
DinnerDinner <singhpranjwal@gmail.com>
2025-09-11 23:15:49 -0400

    Initial commit: Hello World 🚀

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900 text-white text-4xl">
+      Hello World 🚀
+    </div>
+  );
+}
+
+export default App;
