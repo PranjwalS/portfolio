7c5e95619d3707d8637ff3cdd27ffd4c71c29893
DinnerDinner <singhpranjwal@gmail.com>
2025-09-11 23:21:47 -0400

    verceL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-900 text-white text-4xl">
-      MY NAME IS PRANJWAL SINGH!
+      MY NAME IS PRANJWAL SINGH! AND ITS LIVE
     </div>
   );
 }
