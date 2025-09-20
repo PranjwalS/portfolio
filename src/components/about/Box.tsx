2a86cbd8d2e49d2493a5094f81feac25e1f36cc3
DinnerDinner <singhpranjwal@gmail.com>
2025-09-19 23:56:30 -0400

    been busy today, but started designing landing page on figma

---
+++ src/components/about/Box.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface RectangleProps {
+    text: string;
+    color?: string;
+}
+
+const Box: React.FC<RectangleProps> = ({
+    text, 
+    color = "bg-green-600 hover:bg-blue-500"
+}) => {
+    return(
+        <button
+        className={`${color} `}>
+        {text}
+        </button>
+    )
+}
+
+export default Box;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,13 @@
 import Button from "../components/about/Button"
+import Box from "../components/about/Box"
 
 const About: React.FC = () => (
   <div className="p-8">
     <Button text="Hello World!" onClick={() => console.log("HALLO!")} />
+          <Box text="box hehe"/>
+
   </div>
+
 )
 
 
